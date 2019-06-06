8c8c73c911be093967fb6f00b17315f5c50d64b8
Dandelion Mané <decentralion@dandelion.io>
2019-06-06 18:42:15 +0300

    Timeline mimics regular explorer

--- src/explorer/TimelineCredView.js
+++ src/explorer/TimelineCredView.js
@@ -6,6 +6,7 @@ import {Graph, type NodeAddressT} from "../core/graph";
 import {type TimelineScores} from "../analysis/computeTimelineCred.js";
 import {TimelineScoreHelper} from "../analysis/timelineScoreHelper.js";
 import {TimelineCredChart} from "./TimelineCredChart";
+import {format} from "d3-format";
 
 export type Props = {|
   +timelineScores: TimelineScores,
@@ -20,29 +21,49 @@ export class TimelineCredView extends React.Component<Props> {
   render() {
     const {selectedNodeFilter, timelineScores, graph} = this.props;
     const credView = new TimelineScoreHelper(graph, timelineScores);
-    const nodes = credView
-      .credSortedNodes(selectedNodeFilter)
-      .slice(0, MAX_ENTRIES_PER_LIST);
-    const displayedNodes = nodes
+    const nodes = credView.credSortedNodes(selectedNodeFilter);
+    const tableNodes = nodes.slice(0, MAX_ENTRIES_PER_LIST);
+    const chartNodes = nodes
       .slice(0, DEFAULT_ENTRIES_PER_CHART)
       .map((x) => x.node.address);
+    let totalScore = 0;
+    for (const {score} of nodes) {
+      totalScore += score;
+    }
     return (
-      <div>
+      <div style={{width: 1000, margin: "0 auto"}}>
         <TimelineCredChart
           graph={graph}
           timelineScores={timelineScores}
-          displayedNodes={displayedNodes}
+          displayedNodes={chartNodes}
         />
-        {nodes.map(({node, score}) => {
-          return (
-            <div key={node.address}>
-              <span>
-                <Markdown source={node.description} />
-              </span>
-              <span>{score}</span>
-            </div>
-          );
-        })}
+        <table style={{width: 600, margin: "0 auto", padding: "0 10px"}}>
+          <thead>
+            <tr>
+              <th>Node</th>
+              <th style={{textAlign: "right"}}>Cred</th>
+              <th style={{textAlign: "right"}}>% Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableNodes.map(({node, score}) => {
+              return (
+                <tr key={node.address}>
+                  <td>
+                    <Markdown
+                      renderers={{paragraph: "span"}}
+                      source={node.description}
+                    />
+                  </td>
+                  <td style={{textAlign: "right"}}>{format(".1d")(score)}</td>
+                  <td style={{textAlign: "right"}}>
+                    {format(".1%")(score / totalScore)}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
--- src/explorer/TimelineExplorer.js
+++ src/explorer/TimelineExplorer.js
@@ -1,9 +1,10 @@
 // @flow
 
 import React from "react";
+import sortBy from "lodash.sortby";
 import {userNodeType, repoNodeType} from "../plugins/github/declaration";
 import {type RepoId} from "../core/repoId";
-import {Graph, type NodeAddressT} from "../core/graph";
+import {Graph, type NodeAddressT, NodeAddress} from "../core/graph";
 import {
   type PluginDeclaration,
   combineTypes,
@@ -14,6 +15,9 @@ import {
   computeTimelineScores,
 } from "../analysis/computeTimelineCred.js";
 import {TimelineCredView} from "./TimelineCredView";
+import {ProjectDetail} from "./App";
+import {WeightConfig} from "./weights/WeightConfig";
+import {WeightsFileManager} from "./weights/WeightsFileManager";
 
 export type Props = {
   repoId: RepoId,
@@ -22,20 +26,22 @@ export type Props = {
 };
 
 export type State = {
-  filteredNodePrefix: NodeAddressT,
+  selectedNodeTypePrefix: NodeAddressT,
   timelineScores: TimelineScores | null,
   weights: Weights,
   loading: boolean,
+  showWeightConfig: boolean,
 };
 
 const ALPHA = 0.05;
 
 export class TimelineExplorer extends React.Component<Props, State> {
   state = {
-    filteredNodePrefix: userNodeType.prefix,
+    selectedNodeTypePrefix: userNodeType.prefix,
     timelineScores: null,
     weights: defaultWeights(),
     loading: false,
+    showWeightConfig: false,
   };
 
   async analyzeCred() {
@@ -53,23 +59,114 @@ export class TimelineExplorer extends React.Component<Props, State> {
     this.setState({timelineScores, loading: false});
   }
 
+  renderFilterSelect() {
+    const {declarations} = this.props;
+
+    function optionGroup(declaration: PluginDeclaration) {
+      const header = (
+        <option
+          key={declaration.nodePrefix}
+          value={declaration.nodePrefix}
+          style={{fontWeight: "bold"}}
+        >
+          {declaration.name}
+        </option>
+      );
+      const entries = declaration.nodeTypes.map((type) => (
+        <option key={type.prefix} value={type.prefix}>
+          {"\u2003" + type.name}
+        </option>
+      ));
+      return [header, ...entries];
+    }
+    return (
+      <label>
+        <span>Filter by node type: </span>
+        <select
+          value={this.state.selectedNodeTypePrefix}
+          onChange={(e) =>
+            this.setState({selectedNodeTypePrefix: e.target.value})
+          }
+        >
+          <option value={NodeAddress.empty}>Show all</option>
+          {sortBy(declarations, (d) => d.name).map(optionGroup)}
+        </select>
+      </label>
+    );
+  }
+
+  renderConfigurationRow() {
+    const {showWeightConfig} = this.state;
+    const weightFileManager = (
+      <WeightsFileManager
+        weights={this.state.weights}
+        onWeightsChange={(weights: Weights) => {
+          this.setState({weights});
+        }}
+      />
+    );
+    const weightConfig = (
+      <WeightConfig
+        declarations={this.props.declarations}
+        nodeTypeWeights={this.state.weights.nodeTypeWeights}
+        edgeTypeWeights={this.state.weights.edgeTypeWeights}
+        onNodeWeightChange={(prefix, weight) => {
+          this.setState(({weights}) => {
+            weights.nodeTypeWeights.set(prefix, weight);
+            return {weights};
+          });
+        }}
+        onEdgeWeightChange={(prefix, weight) => {
+          this.setState(({weights}) => {
+            weights.edgeTypeWeights.set(prefix, weight);
+            return {weights};
+          });
+        }}
+      />
+    );
+    return (
+      <div>
+        <div style={{marginTop: 10, display: "flex"}}>
+          {this.renderFilterSelect()}
+          <span style={{flexGrow: 1}} />
+          {weightFileManager}
+          <button
+            onClick={() => {
+              this.setState(({showWeightConfig}) => ({
+                showWeightConfig: !showWeightConfig,
+              }));
+            }}
+          >
+            {showWeightConfig
+              ? "Hide weight configuration"
+              : "Show weight configuration"}
+          </button>
+        </div>
+        {showWeightConfig && <div style={{marginTop: 10}}>{weightConfig}</div>}
+      </div>
+    );
+  }
+
   render() {
     const analyzeButton = (
       <button disabled={this.state.loading} onClick={() => this.analyzeCred()}>
         Analyze
       </button>
     );
+
     const timelineCredView = this.state.timelineScores != null && (
       <TimelineCredView
         timelineScores={this.state.timelineScores}
-        selectedNodeFilter={this.state.filteredNodePrefix}
+        selectedNodeFilter={this.state.selectedNodeTypePrefix}
         graph={this.props.graph}
       />
     );
     return (
-      <div>
-        {timelineCredView}
+      <div style={{width: 900, margin: "0 auto"}}>
+        <ProjectDetail repoId={this.props.repoId} />
         {analyzeButton}
+        {this.renderConfigurationRow()}
+        {timelineCredView}
       </div>
     );
   }
